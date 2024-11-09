74f568e4b1b0e3b27801046e789f63af87df05a4
Adam Ibrahim <adamyehialy@gmail.com>
2024-11-09 22:19:28 +0400

    inital2

--- README.old.md
+++
@@ -1,2 +0,0 @@
-# AIExamify
- A web app that uses AI to create custom quizzes and flashcards from uploaded educational materials. It enhances the learning experience by making study aids interactive and effective.
--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import "./index.css";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const BookList = () => {
+  return (
+    <section>
+      <Book />
+    </section>
+  );
+};
+//can have explanation for each question
+const Book = () => {
+  return (
+    <article>
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+};
+const Image = () => <h2>i</h2>;
+const Author = () => <h2>a</h2>;
+const Title = () => <h2>t</h2>;
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<BookList />);
