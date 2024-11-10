a07285aa9021b55376cb123c318fcbf1f7e01739
Adam Ibrahim <adamyehialy@gmail.com>
2024-11-10 15:26:27 +0400

    i

--- src/index.js
+++ src/index.js
@@ -1,28 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
 import "./index.css";
+import App from "./App";
 
-const BookList = () => {
-  return (
-    <section>
-      <Book />
-    </section>
-  );
-};
-//can have explanation for each question
-const Book = () => {
-  return (
-    <article>
-      <Image />
-      <Title />
-      <Author />
-    </article>
-  );
-};
-const Image = () => <h2>i</h2>;
-const Author = () => <h2>a</h2>;
-const Title = () => <h2>t</h2>;
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(<BookList />);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
