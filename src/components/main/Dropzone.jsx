e4e45bdcb4ed295d49b4ece8ae404adb8b13d274
adam-a-i <adamyehialy@gmail.com>
2024-11-19 09:21:33 +0400

    adding functionality to file acception(still need to work on ui for it)

--- src/components/main/Dropzone.jsx
+++ src/components/main/Dropzone.jsx
@@ -1,14 +1,16 @@
-import React, { useCallback } from 'react'
+import React, { useCallback, useState } from 'react'
 import { useDropzone } from 'react-dropzone'
 import UploadIcon from '../../assets/UploadIcon.jsx'
 // NEXT WE WANT TO MAKE IT SO THAT WE UPLOAD A FILE
 // THEN WE TAKE THIS FILE AND EXTRACT THE TEXT FROM IT USING A LIBRARY
 //THEN WE PASS THIS FILE THROUGH AN API
 const Dropzone = () => {
+    const [FileAccept, setFileAccept] = useState(false);
     const onDrop = useCallback(
       (acceptedFiles) => { // files uploaded found here
         console.log(acceptedFiles);
-      },
+         setFileAccept(true);
+        console.log(FileAccept);},
       [],
     )
     
@@ -21,6 +23,12 @@ const Dropzone = () => {
             'application/vnd.openxmlformats-officedocument.presentationml.presentation': ['.pptx'], // For PPTX
           }
     })
+
+  if(FileAccept){
+    return(
+      <div>ads</div>
+    );
+  }
   return (
     <div>
         <p className='help'>Try it out by uploading a screenshot, photo, or entering your text content below. It even works with handwritten content.</p>
@@ -33,8 +41,7 @@ const Dropzone = () => {
                     <div className='upload-wrapper'>
                     <UploadIcon />
                     <p className='upload-text'> Drag a document here or click to browse</p></div>
-                }
-                
+}               
             </div>
         </div>
     </div>
