e3cd969c288cd0596db4651182e5b4ef55711993
Adam Ibrahim <adamyehialy@gmail.com>
2024-12-01 22:57:32 +0400

    timer, check, and qs number hook

--- src/components/main/quiz/QuizPage.jsx
+++ src/components/main/quiz/QuizPage.jsx
@@ -1,15 +1,29 @@
 import React from 'react'
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import Timer from '../../../assets/timer';
 const QuizPage = ({ quiz }) => {  
+  const [currentQuestion, setCurrentQuestion] = useState(0);
   
-  
+
+  const handleQuestionChange = () =>{
+    if(currentQuestion <quiz.quiz.length) setCurrentQuestion(currentQuestion+1);
+  }
+
+  const checkAnswer = () => {
+    console.log("hi");
+  }
+
   return (
     <div className="quiz">
       <h1>Generated Quiz</h1>
       <div className="question">
         {quiz.quiz?.map((question, index) => { // ensure quiz.quiz is defined
           return (
+            <>
             <div key={index} className="questionText">
+            <div className='timer-wrapper'>
+              <Timer/>
+            </div>
               <h3>{question.question}</h3>
               <div className="options">
                 {question.options.map((option, idx) => {
@@ -23,9 +37,11 @@ const QuizPage = ({ quiz }) => {
                     </div>
                   );
                 })}
+                <button className='check' onClick={() => checkAnswer()}> Check </button>
               </div>
               <p>Correct Answer: {question.correct_answer}</p>
             </div>
+            </>
           );
         })}
       </div>
