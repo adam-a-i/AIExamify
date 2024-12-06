03fede13662061ce43db14aa8ed92a6939b17679
Adam Ibrahim <adamyehialy@gmail.com>
2024-12-06 20:06:21 +0400

    confetti component added

--- src/components/main/quiz/Result.jsx
+++ src/components/main/quiz/Result.jsx
@@ -1,16 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { useLocation } from 'react-router-dom' // to receeive props from prev page
+import Confetti from 'react-confetti';
+
+
 const Result = () => {
     const location = useLocation();
     const {correctAnswers, minutes, seconds} = location.state || {};
+
+    const [confettiPieces, setConfettiPieces] = useState(300);
+    useEffect(() => {
+      const interval = setInterval(() => {
+        setConfettiPieces((prev) => Math.max(prev - 10, 0)); // reduce pieces of confetti
+      }, 200);
+      return () => clearInterval(interval);
+    }, []);
+    console.log('Confetti Pieces:', confettiPieces);
   return (
-    <div>
+    <>
+      {<Confetti numberOfPieces={confettiPieces} width={window.innerWidth} height={document.documentElement.scrollHeight} />}
       <h1>Quiz Results</h1>
       <p>Correct Answers: {correctAnswers}</p>
       <div className='timer-text'>
                   <span>{String(minutes).padStart(2,'0')}</span>:<span>{String(seconds).padStart(2,'0')}</span>
                 </div>
-    </div>
+    </>
   )
 }
 
