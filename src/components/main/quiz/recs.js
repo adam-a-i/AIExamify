fc8ca7009691a5a6b2629345aea228a31e94fcb2
Adam Ibrahim <adamyehialy@gmail.com>
2024-12-07 22:39:06 +0400

    implemented for calling

--- src/components/main/quiz/recs.js
+++ src/components/main/quiz/recs.js
@@ -1,36 +1,16 @@
-export const txt = async (setQuiz, fileInfo, setText, setLoading,navigate) => {
-  const file = fileInfo[0];
-  if (file) { 
-    const reader = new FileReader();
-    reader.onload = async (event) => {
-      const arrayBuffer = event.target.result;
-      try {
-        const result = await mammoth.convertToHtml({ arrayBuffer });
-        const html = removeImageTags(result.value);
-        const pureText = convert(html, { wordwrap: false });
-
-        setText(pureText);
-
-        // Fetch quiz from the backend
-        const response = await fetch('http://localhost:3000/generate-quiz', {
+export const txt = async (quiz, incorrectAnswers) => {
+    const quizJson = JSON.stringify(quiz);
+    const incorrectAnswersJSON = JSON.stringify(incorrectAnswers);
+    try {
+        const response = await fetch('http://localhost:3000/generate-keywords', {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify({ inputText: pureText }),
+          body: JSON.stringify({quiz: quizJson, incorrectAnswers: incorrectAnswersJSON}),
         });
         const data = await response.json();
         console.log('Quiz Output:', data.quiz);
         setQuiz(data.quiz);
       } catch (error) {
-        console.error('Error processing file:', error);
-      } finally{
-          // implement set interval here just to ensure that everything goes right and to have loading screen for longer
-          setTimeout(()=>{
-            setLoading(false);
-            navigate('/quiz');//navigating to quiz page after the 9 seconds complete
-          }, 2000)
-
+        console.error('error generating recs');
       }
-    };
-    reader.readAsArrayBuffer(file);
-  }
-};
+    }
